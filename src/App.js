54d5fcd5383eadba3c980291bca02c4ee651a427
AmanJaiswal99 <aman2000.jaiswal@gmail.com>
2025-05-03 15:06:31 +0530

    F-10 added the dino game path and modified the website UI to look like a personal website with about and blogs and stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Tictactoe from './components/games/Tictactoe';
 import Homepage from './components/pages/Homepage';
 import Navbar from './components/elements/Navbar';
